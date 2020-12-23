98dd94c857ee44eae66d57762b7e1ca158262a57
Prateek Sinha <prateeksinha18@gmail.com>
2020-12-23 20:05:35 +0530

    part 1 completed

--- assignment4.java
+++ assignment4.java
@@ -2,65 +2,185 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class edge{
+class node{
 	public String Id, Label;
+
+	public node(String Id, String Label){
+		this.Id = Id;
+		this.Label = Label;
+	}
+
+	public String getId(){
+		return this.Id;
+	}
+
+	public String getLabel(){
+		return this.Label;
+	}
 }
 
-public class node{
-	public String Source,Target, Weight;
+
+class sizeNname{
+	int size;
+	String name;
+
+	public sizeNname(int size, String name){
+		this.name = name;
+		this.size = size;
+	}
 }
 
-public class Graph{
+class edge{
+	public String Source,Target;
+	int Weight;
+
+	public edge(String Source, String Target, int Weight){
+		this.Source = Source;
+		this.Target = Target;
+		this.Weight = Weight;
+	}
+
+	public String getS(){
+		return this.Source;
+	}
+
+	public String getT(){
+		return this.Target;
+	}
+	public int getW(){
+		return this.Weight;
+	}
+}
+
+class Graph{
 	ArrayList<ArrayList<String>> adj;   // adjacency list of the the given graph
 	ArrayList<edge> edgelist;			// list of edges
 	ArrayList<node> nodelist;			// list of nodes
 
-	public double AVG(){				// Part 1
-		return roundOff = Math.round(( (edgelist.size()) / (nodelist.size())) * 100.0) / 100.0;
+	public Graph(ArrayList<edge> edgelist, ArrayList<node> nodelist){
+		this.edgelist = edgelist;
+		this.nodelist = nodelist;
+		this.adj = new ArrayList<ArrayList<String>>();
+		 for (int i = 0; i < nodelist.size(); i++) {
+      		ArrayList<String> adjnodes = new ArrayList<String>();
+      		for(int j = 0; j< edgelist.size(); j++){
+      			if(edgelist.get(j).getS() == nodelist.get(i).getId()){
+      				adjnodes.add(edgelist.get(j).getT());
+      			}
+      		}
+      		adj.add(adjnodes);
+    	}
+
 	}
 
-	public void qsort(int[] arr, int l, int r){
+	public void aver(){				// Part 1
+		double a = ((double)this.edgelist.size())/this.nodelist.size();
+		double roundOff = Math.round(a*100.0)/100.0;
+
+    	System.out.println(roundOff);
+	}
+
+	public void qsort(ArrayList<sizeNname> arr, int l, int r){
 		if(l>=r){
 			return;
 		}
 
-		int p = arr[l];
+		sizeNname p = arr.get(l);
 		int i = l;
 		int j = r;
 		while(i < j && i <=r ){
-			while(i <= r && arr[i] <= p){
+			while(i <= r && (arr.get(i).size > p.size || (arr.get(i).size == p.size && arr.get(i).name.compareTo(p.name) >= 0))){
 				i++;
 			}
-			while(arr[j] > p && j >=l ){
+			while(arr.get(j).size < p.size && j >=l ){
 				j--;
 			}
 			if(i<j){
-				int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+				sizeNname temp = arr.get(i);
+                arr.get(i).size = arr.get(j).size;
+                arr.get(i).name = arr.get(j).name;
+                arr.get(j).size = temp.size;
+                arr.get(j).name = temp.name;
 			}
+			System.out.println(i);
 					
 		}
-		arr[l] = arr[j];
-		arr[j] = p;
-		quicksort(arr,l,j-1);
-		quicksort(arr,j+1,r);
+		arr.get(l).size = arr.get(j).size;
+		arr.get(l).name = arr.get(j).name;
+		
+		arr.get(j).size = p.size;
+		arr.get(j).name = p.name;
+		qsort(arr,l,j-1);
+		qsort(arr,j+1,r);
 	}
 		
 	
 
 	public void rank(){					// Part 2
 		int n = adj.size();
-		int[] size = new int[n];
+		ArrayList<sizeNname> sNn = new ArrayList<>();
 		for(int i=0; i<n; i++){			// size array
-			size[i] = adj.get(i).size();
+			sizeNname temp = new sizeNname(adj.get(i).size(), adj.get(i).get(0));
+			sNn.add(temp);
 		}
-		this.qsort(size, 0, n-1);		// sorting acc. to size 
-		for(int i=0; i<n: i++){
-			System.out.print(adj.get(i).Label + ",");	
+
+		for(int i=0; i<n; i++){
+			System.out.print(sNn.get(i).size + ",");	
+		}
+		// this.qsort(sNn, 0, n-1);		// sorting acc. to size 
+		for(int i=0; i<n; i++){
+			System.out.print(sNn.get(i).name + ",");	
 		}					
 	} 
 
 
 }
 
+public class assignment4{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		ArrayList<node> nodelist = new ArrayList<node>();
+		ArrayList<edge> edgelist = new ArrayList<edge>();
+		node temp = new node("aa","aa");
+		nodelist.add(temp);
+		temp = new node("bb","bb");
+		nodelist.add(temp);
+		temp = new node("cc","cc");
+		nodelist.add(temp);
+		temp = new node("dd","dd");
+		nodelist.add(temp);
+		temp = new node("ee","ee");
+		nodelist.add(temp);
+		temp = new node("ff","ff");
+		nodelist.add(temp);
+		temp = new node("gg","gg");
+		nodelist.add(temp);
+
+
+		edge temp2 = new edge("aa","bb",1);
+		edgelist.add(temp2);
+		temp2 = new edge("aa","dd",1);
+		edgelist.add(temp2);
+		temp2 = new edge("bb","aa",1);
+		edgelist.add(temp2);
+		temp2 = new edge("bb","dd",1);
+		edgelist.add(temp2);
+		temp2 = new edge("cc","aa",1);
+		edgelist.add(temp2);
+		temp2 = new edge("dd","aa",1);
+		edgelist.add(temp2);
+		temp2 = new edge("dd","bb",1);
+		edgelist.add(temp2);
+		temp2 = new edge("ee","ff",1);
+		edgelist.add(temp2);
+		temp2 = new edge("ff","gg",1);
+		edgelist.add(temp2);
+		temp2 = new edge("gg","ee",1);
+		edgelist.add(temp2);
+		
+
+		Graph G = new Graph(edgelist,nodelist);
+		G.aver();
+		G.rank();
+	}
+}
